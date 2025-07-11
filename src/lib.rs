627d504a331cfc7c0bfeb24a3b585e1866e454f9
thedoctor <thedoctor095@yahoo.com>
2025-07-11 18:38:49 +0300

    Adds defined type

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ pub mod parser;
 use parser::{Properties, fetch_temps};
 use std::{collections::HashMap, iter::from_fn, thread::sleep, time::Duration};
 
-pub fn from_stream(interval: Option<u64>) -> impl Iterator<Item = Option<HashMap<String, Vec<Properties>>>>{
+pub type HWProperties = Option<HashMap<String, Vec<Properties>>>;
+
+pub fn from_stream(interval: Option<u64>) -> impl Iterator<Item = HWProperties>{
     let interval = interval.unwrap_or(0) * 1000;
     let mut first_iter = true;
     from_fn(move || {
@@ -16,6 +18,6 @@ pub fn from_stream(interval: Option<u64>) -> impl Iterator<Item = Option<HashMap
     })
 }
 
-pub fn from_func() -> Option<HashMap<String, Vec<Properties>>> {
+pub fn from_func() -> HWProperties {
     return fetch_temps();
 }
